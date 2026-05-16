ccc30ec367c480bd597b02e905ac23b288faf9fe
Aman Anwar <aman.anwar18oct@gmail.com>
2026-05-16 15:08:47 -0400

    add: bg and funts font

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,9 @@
 export function App() {
   return (
-    <main className="h-screen w-screen overflow-hidden p-5">
-      <div className="h-full w-full rounded-[32px] bg-yellow-300" />
+    <main className="h-screen w-screen overflow-hidden">
+      <div className="flex h-full w-full items-center justify-center bg-linear-to-b from-[#0072cd] to-[#60baff]">
+        <h1 className="funts-font text-6xl text-white">Funts</h1>
+      </div>
     </main>
   )
 }
